8f118140ef31854db05e54e758ae6366c4f9728b
smutallib97 <smutallib97@gmail.com>
2022-10-11 23:56:18 +0530

    Added: Maxmim number from 3 float number

--- TestMaximum.java
+++ TestMaximum.java
@@ -16,8 +16,24 @@ public class TestMaximum {
             }
         }
     }
+    public static Float compareTo(Float a,Float b,Float c) {
+        if(a > b){
+            if(a > c) {
+                return a;
+            } else {
+                return c;
+            }
+        } else {
+            if(b > c) {
+                return b;
+            } else {
+                return c;
+            }
+        }
+    }
     public static void main(String[] args) {
 
-        System.out.println("Maximum number from 3 Integer Object : "+compareTo(25,18,38));
+        System.out.println("Maximum  number from 3 Integer Object : "+compareTo(25,18,38));
+        System.out.println("Maximum float number from 3 Float Object : "+compareTo(25.0F,18.0F,38.0F));
     }
 }
