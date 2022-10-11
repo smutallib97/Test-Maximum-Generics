71ae20be04df832d161da70eef9133dd84e4dbe6
smutallib97 <smutallib97@gmail.com>
2022-10-12 00:03:24 +0530

    Added: Maximum String from 3 String value

--- TestMaximum.java
+++ TestMaximum.java
@@ -31,9 +31,27 @@ public class TestMaximum {
             }
         }
     }
+    public static String compareTo(String str1,String str2,String str3) {
+        if(str1.compareTo(str2) > 0) {
+            if(str1.compareTo(str3) > 0) {
+                return str1;
+            } else {
+                return str3;
+            }
+        } else {
+            if(str2.compareTo(str3) > 0) {
+                return str2;
+            } else {
+                return str3;
+            }
+        }
+    }
     public static void main(String[] args) {
 
-        System.out.println("Maximum  number from 3 Integer Object : "+compareTo(25,18,38));
+        System.out.println("Welcome to Java Core - Generics Test Maximum.");
+
+        System.out.println("Maximum number from 3 Integer Object : "+compareTo(25,18,38));
         System.out.println("Maximum float number from 3 Float Object : "+compareTo(25.0F,18.0F,38.0F));
+        System.out.println("Maximum String from 3 String Object : "+compareTo("Nitish","Ullas","Shivam"));
     }
 }
