a4881f8ce3cf7d8b1db1ea29283d0b3ea0c85524
smutallib97 <smutallib97@gmail.com>
2022-10-12 00:11:55 +0530

    Added: Refactor of code for using Generic Class

--- TestMaximum.java
+++ TestMaximum.java
@@ -1,57 +1,31 @@
 package com.bridgelabz;
 
 public class TestMaximum {
-    public static Integer compareTo(Integer a,Integer b,Integer c) {
-        if(a > b){
-            if(a > c) {
+    public <T extends Comparable<T>> T compareTo(T a,T b,T c) {
+        if(a.compareTo(b) > 0){
+            if(a.compareTo(c) > 0) {
                 return a;
             } else {
                 return c;
             }
         } else {
-            if(b > c) {
+            if(b.compareTo(c) > 0) {
                 return b;
             } else {
                 return c;
             }
         }
     }
-    public static Float compareTo(Float a,Float b,Float c) {
-        if(a > b){
-            if(a > c) {
-                return a;
-            } else {
-                return c;
-            }
-        } else {
-            if(b > c) {
-                return b;
-            } else {
-                return c;
-            }
-        }
-    }
-    public static String compareTo(String str1,String str2,String str3) {
-        if(str1.compareTo(str2) > 0) {
-            if(str1.compareTo(str3) > 0) {
-                return str1;
-            } else {
-                return str3;
-            }
-        } else {
-            if(str2.compareTo(str3) > 0) {
-                return str2;
-            } else {
-                return str3;
-            }
-        }
-    }
+
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Java Core - Generics Test Maximum.");
-
-        System.out.println("Maximum number from 3 Integer Object : "+compareTo(25,18,38));
-        System.out.println("Maximum float number from 3 Float Object : "+compareTo(25.0F,18.0F,38.0F));
-        System.out.println("Maximum String from 3 String Object : "+compareTo("Nitish","Ullas","Shivam"));
+        System.out.println("Welcome to Test Maximum Program");
+        
+        TestMaximum integerComparison = new TestMaximum();
+        System.out.println("Maximum number from 3 Integer Object : "+integerComparison.compareTo(25,18,38));
+        TestMaximum floatComparison = new TestMaximum();
+        System.out.println("Maximum float number from 3 Float Object : "+floatComparison.compareTo(25.0F,18.0F,38.0F));
+        TestMaximum stringComparison = new TestMaximum();
+        System.out.println("Maximum String from 3 String Object : "+stringComparison.compareTo("Nitish","Ullas","Shivam"));
     }
 }
