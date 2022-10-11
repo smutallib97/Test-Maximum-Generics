a3460da3bb43930b583c03013334752df209db77
smutallib97 <smutallib97@gmail.com>
2022-10-11 23:49:52 +0530

    Added: Maxmim number from 3 integer number

--- TestMaximum.java
+++ TestMaximum.java
@@ -1,7 +1,23 @@
 package com.bridgelabz;
 
 public class TestMaximum {
+    public static Integer compareTo(Integer a,Integer b,Integer c) {
+        if(a > b){
+            if(a > c) {
+                return a;
+            } else {
+                return c;
+            }
+        } else {
+            if(b > c) {
+                return b;
+            } else {
+                return c;
+            }
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome to Test Maximum Program");
+
+        System.out.println("Maximum number from 3 Integer Object : "+compareTo(25,18,38));
     }
 }
